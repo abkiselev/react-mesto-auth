4a0a2c94068dbd0fc6cfba8f966132a04914c188
Киселев Алексей Борисович <a.b.kiselev@yandex.ru>
2022-07-28 08:47:06 +0300

    Испралена ошибка 2-го ревью

--- src/components/Forma.js
+++ src/components/Forma.js
@@ -1,49 +1,27 @@
-import { useState, useEffect } from 'react';
-
 function Forma({ children, title, name, onSubmit, isFormValid, submitButtonText, onClose, loggedIn }) {    
-    const viewPopup = {
-        container: 'popup__container',
-        title: 'popup__title',
-        form: `${name} popup__form`,
-        button: 'popup__button',
-        buttonDisabled: 'popup__button_disabled',
-        closeButton: 'popup__close',
-    }
-    
-    const viewStatic = {
-        container: 'static-form',
-        title: 'static-form__title',
-        form: `${name} static-form__form`,
-        button: 'static-form__button',
-        buttonDisabled: 'static-form__button_disabled',
-        closeButton: '_not_visible',
-    }
-    
-    const [formState, setFormState] = useState({});
-    
-    useEffect(() => {
-        if(!loggedIn){
-            setFormState(viewStatic)
-        } else {setFormState(viewPopup)}
-    }, []);
-    
-    
     
     return (
-            
-            <div className={formState.container}>
-                <h2 className={formState.title}>
-                {title}
-                </h2>
-                <form name={name} className={formState.form} onSubmit={onSubmit} noValidate>
+
+            <div className={loggedIn ? 'popup__container' : 'static-form'}>
+
+                <h2 className={loggedIn ? 'popup__title' : 'static-form__title'}>{title}</h2>
+
+                <form name={name} className={loggedIn ? `${name} popup__form` : `${name} 'static-form__form'`} onSubmit={onSubmit} noValidate>
 
                     {children}
 
-                    <button type="submit" className={`${formState.button} ${!isFormValid && `${formState.buttonDisabled}`}`}>{submitButtonText}</button>
+                    <button
+                    type='submit'
+                    className={`${loggedIn ? 'popup__button' : 'static-form__button'}
+                                ${!isFormValid && `${loggedIn ? 'popup__button_disabled' : 'static-form__button_disabled'}`}`}
+                    >{submitButtonText}</button>
+
                 </form>
-                <button className={formState.closeButton} type="button" onClick={onClose} />
-            </div>
                 
+                <button className={loggedIn ? 'popup__close' : '_not_visible'} type='button' onClick={onClose} />
+
+            </div>
+                            
     );
 }
 
